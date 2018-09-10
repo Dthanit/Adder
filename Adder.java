ca8b5d1e38e75164ec0c82ff6f56eaca1dfbd727
Natthanit Jaipho <60050160@kmitl.ac.th>
2018-09-10 19:16:55 +0700

    add file

---
+++ Adder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Adder{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a = ");
+        int a = sc.nextInt();
+        System.out.print("Enter b = ");
+        int b = sc.nextInt();
+       
+        int Ans = a+b;
+        System.out.println(Ans);   
+    }
+}
